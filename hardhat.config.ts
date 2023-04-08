1934d800a10d95baf314996463b2213ba742a145
Alex Pedersen <mails@alexpedersen.dev>
2023-04-08 13:00:44 +0200

    update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-};
+  defaultNetwork: 'ronin',
+  networks: {
+    ronin: {
+      chainId: 2020,
+      url: 'https://api.roninchain.com/rpc',
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+    saigon: {
+      chainId: 2021,
+      url: 'https://saigon-testnet.roninchain.com/rpc',
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
+}
 
-export default config;
+export default config
