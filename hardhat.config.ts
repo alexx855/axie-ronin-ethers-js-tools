51e642d33985c9fdadafbc5297905c124760524b
Alex Pedersen <mails@alexpedersen.dev>
2023-04-12 22:19:10 +0200

    feat: add account tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,59 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as fs from 'fs/promises'
+import {
+  CONTRACT_WETH_ADDRESS,
+  CONTRACT_AXIE_ADDRESS,
+  CONTRACT_AXIE_ABI_JSON_PATH,
+} from "./lib/constants";
+
 dotenv.config()
+
+export type AvailableNetworks = 'ronin' | 'saigon'
+
+task('account', 'Get info of the deployer account', async (taskArgs, hre) => {
+  if (hre.network.name != 'ronin' && hre.network.name != 'saigon') {
+    throw new Error('Network not supported')
+  }
+
+  try {
+    const network: AvailableNetworks = hre.network.name
+    const accounts = await hre.ethers.getSigners()
+    const signer = accounts[0]
+    const address = signer.address.toLowerCase()
+    console.log('Address:', address.replace('0x', 'ronin:'))
+
+    // get RON balance
+    const balance = await hre.ethers.provider.getBalance(address)
+    const balanceInEther = hre.ethers.utils.formatEther(balance)
+    console.log('RON:', balanceInEther)
+
+    // get WETH balance
+    const wethABI = JSON.parse(await fs.readFile(CONTRACT_AXIE_ABI_JSON_PATH, 'utf8'))
+    const wethContract = new hre.ethers.Contract(CONTRACT_WETH_ADDRESS[network], wethABI, signer)
+    const wethBalance = await wethContract.balanceOf(address)
+    const wethBalanceInEther = hre.ethers.utils.formatEther(wethBalance)
+    console.log('WETH:', wethBalanceInEther)
+
+    // get axie contract
+    const axieABI = JSON.parse(await fs.readFile(CONTRACT_AXIE_ABI_JSON_PATH, 'utf8'))
+    const axieContract = await new hre.ethers.Contract(CONTRACT_AXIE_ADDRESS[network], axieABI, signer)
+
+    // get axies balance for the address
+    const axiesBalance = await axieContract.balanceOf(address)
+    console.log('Axies:', hre.ethers.BigNumber.from(axiesBalance).toString())
+
+    // get USDC balance
+    const usdcABI = JSON.parse(await fs.readFile(CONTRACT_AXIE_ABI_JSON_PATH, 'utf8'))
+    const usdcContract = new hre.ethers.Contract(CONTRACT_WETH_ADDRESS[network], usdcABI, signer)
+    const usdcBalance = await usdcContract.balanceOf(address)
+    const usdcBalanceInEther = hre.ethers.utils.formatUnits(usdcBalance, 6)
+    console.log('USDC balance: ', usdcBalanceInEther)
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,31 +1,67 @@
-export const CONTRACT_AXIE_ADDRESS = '0x32950db2a7164ae833121501c797d79e7b79d74c'
-export const CONTRACT_AXIE_ABI_JSON_PATH = 'lib/abis/axie.json'
-
-export const CONTRACT_SLP_ADDRESS = '0xa8754b9fa15fc18bb59458815510e40a12cd2014'
-export const CONTRACT_SLP_ABI_JSON_PATH = 'lib/abis/slp.json'
-
-export const CONTRACT_WETH_ADDRESS = '0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5'
-export const CONTRACT_WETH_ABI_JSON_PATH = 'lib/abis/weth.json'
-
-export const CONTRACT_AXS_ADDRESS = '0x97a9107c1793bc407d6f527b77e7fff4d812bece'
-export const CONTRACT_AXS_ABI_JSON_PATH = 'lib/abis/axs.json'
-
-export const CONTRACT_RUNE_ADDRESS = '0xc25970724f032af21d801978c73653c440cf787c'
-export const CONTRACT_RUNE_ABI_JSON_PATH = 'lib/abis/rune.json'
-
-export const CONTRACT_CHARM_ADDRESS = '0x814a9c959a3ef6ca44b5e2349e3bba9845393947'
-export const CONTRACT_CHARM_ABI_JSON_PATH = 'lib/abis/charm.json'
-
-export const CONTRACT_AXIE_GENES_BREWER_ADDRESS = '0x54e91daf9362900f94d32bd084beff4bdb73ea62'
-export const CONTRACT_AXIE_GENES_BREWER_ABI_JSON_PATH = 'lib/abis/genes_brewer.json'
-
-export const CONTRACT_MULTICALL_ADDRESS = '0xc76d0d0d3aa608190f78db02bf2f5aef374fc0b9'
-export const CONTRACT_MULTICALL_ABI_JSON_PATH = 'lib/abis/multicall.json'
-
-export const CONTRACT_ERC721_BATCH_TRANSFER_ADDRESS = '0x2368dfed532842db89b470fde9fd584d48d4f644'
-export const CONTRACT_ERC721_BATCH_TRANSFER_ABI_JSON_PATH = 'lib/abis/erc721_batch_transfer.json'
-
-export const CONTRACT_MARKETPLACE_V2_ADDRESS = '0xfff9ce5f71ca6178d3beecedb61e7eff1602950e'
-export const CONTRACT_MARKETPLACE_V2_ABI_JSON_PATH = 'lib/abis/market_v2.json'
-
 export const GRAPHQL_URL = 'https://graphql-gateway.axieinfinity.com/graphql'
+
+export const CONTRACT_AXIE_ABI_JSON_PATH = 'lib/abis/axie.json'
+export const CONTRACT_AXIE_ADDRESS = {
+  'ronin': '0x32950db2a7164ae833121501c797d79e7b79d74c',
+  'saigon': '0xcaca1c072d26e46686d932686015207fbe08fdb8'
+}
+
+export const CONTRACT_SLP_ABI_JSON_PATH = 'lib/abis/slp.json'
+export const CONTRACT_SLP_ADDRESS = {
+  'ronin': '0xa8754b9fa15fc18bb59458815510e40a12cd2014',
+  'saigon': '0x82f5483623d636bc3deba8ae67e1751b6cf2bad2'
+}
+
+export const CONTRACT_WETH_ABI_JSON_PATH = 'lib/abis/weth.json'
+export const CONTRACT_WETH_ADDRESS = {
+  'ronin': '0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5',
+  'saigon': '0x29c6f8349a028e1bdfc68bfa08bdee7bc5d47e16'
+}
+
+export const CONTRACT_AXS_ABI_JSON_PATH = 'lib/abis/axs.json'
+export const CONTRACT_AXS_ADDRESS = {
+  'ronin': '0x97a9107c1793bc407d6f527b77e7fff4d812bece',
+  'saigon': '0x3c4e17b9056272ce1b49f6900d8cfd6171a1869d'
+}
+
+export const CONTRACT_USDC_ABI_JSON_PATH = 'lib/abis/weth.json'
+export const CONTRACT_USDC_ADDRESS = {
+  'ronin': '',
+  'saigon': '0x067fbff8990c58ab90bae3c97241c5d736053f77'
+}
+
+export const CONTRACT_RUNE_ABI_JSON_PATH = 'lib/abis/rune.json'
+export const CONTRACT_RUNE_ADDRESS = {
+  'ronin': '0xc25970724f032af21d801978c73653c440cf787c',
+  'saigon': '0xe9803c8943ee28f06a926206e2387d7f3e32bdbc'
+}
+
+export const CONTRACT_CHARM_ABI_JSON_PATH = 'lib/abis/charm.json'
+export const CONTRACT_CHARM_ADDRESS = {
+  'ronin': '0x814a9c959a3ef6ca44b5e2349e3bba9845393947',
+  'saigon': '0xdab4b1863e132bcdd853635db7848f6c5f442cc8'
+}
+
+export const CONTRACT_MARKETPLACE_V2_ABI_JSON_PATH = 'lib/abis/market_v2.json'
+export const CONTRACT_MARKETPLACE_V2_ADDRESS = {
+  'ronin': '0xfff9ce5f71ca6178d3beecedb61e7eff1602950e',
+  'saigon': '0xa7be1f3c47f191217a6e0bf3f7d05ca39427e0af'
+}
+
+export const CONTRACT_AXIE_GENES_BREWER_ABI_JSON_PATH = 'lib/abis/genes_brewer.json'
+export const CONTRACT_AXIE_GENES_BREWER_ADDRESS = {
+  'ronin': '0x54e91daf9362900f94d32bd084beff4bdb73ea62',
+  // 'saigon': ''
+}
+
+export const CONTRACT_MULTICALL_ABI_JSON_PATH = 'lib/abis/multicall.json'
+export const CONTRACT_MULTICALL_ADDRESS = {
+  'ronin': '0xc76d0d0d3aa608190f78db02bf2f5aef374fc0b9',
+  // 'saigon': ''
+}
+
+export const CONTRACT_ERC721_BATCH_TRANSFER_ABI_JSON_PATH = 'lib/abis/erc721_batch_transfer.json'
+export const CONTRACT_ERC721_BATCH_TRANSFER_ADDRESS = {
+  'ronin': '0x2368dfed532842db89b470fde9fd584d48d4f644',
+  // 'saigon': ''
+}
