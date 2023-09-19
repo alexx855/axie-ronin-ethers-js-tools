572e2b86eb01e39d41ca3dc3dc30344993fd1f9c
Alex Pedersen <hello@alexpedersen.dev>
2023-09-19 23:13:25 +0200

    chores: add buy example

---
+++ examples/buy/README.md
@@ -0,0 +1,13 @@
+# Example: Buy axie from marketplace
+
+Copy the `.env.example` file to `.env` and fill in your account private key (you can get this from the Ronin wallet) and yout SkyMavis API Key (get from <https://developers.skymavis.com/console/applications/>). Please not share your private key with anyone.
+
+## Install
+
+```npm install```
+
+### Run
+
+```npm run start 4978715```
+
+4978715 is the axie id to buy used in this example, you can change it to any axie id you want to buy, it must be listed in the marketplace.
---
+++ examples/buy/index.ts
@@ -0,0 +1,49 @@
+import { buyMarketplaceOrder, getWETHContract } from "axie-ronin-ethers-js-tools";
+import { ethers } from 'ethers';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+async function buyAxie() {
+
+  if (!process.env.PRIVATE_KEY || !process.env.SKIMAVIS_DAPP_KEY) {
+    throw new Error('Please set your PRIVATE_KEY and SKIMAVIS_DAPP_KEY in a .env file')
+  }
+
+  // see https://docs.skymavis.com/api/rpc
+  const connection = {
+    url: 'https://api-gateway.skymavis.com/rpc',
+    headers: {
+      'x-api-key': process.env.SKIMAVIS_DAPP_KEY
+    }
+  }
+
+  const provider = new ethers.providers.JsonRpcProvider(connection);
+
+  // Import the wallet private key from the environment
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+
+  const address = await wallet.getAddress()
+  console.log(`Wallet address: ${address}`)
+
+  // get RON balance
+  const balance = await provider.getBalance(address)
+  const balanceInEther = ethers.utils.formatEther(balance)
+  console.log(`Balance: ${balanceInEther} RON`)
+
+  // get WETH balance
+  const wethContract = await getWETHContract(provider)
+  const wethBalance = await wethContract.balanceOf(address)
+  const wethBalanceInEther = ethers.utils.formatEther(wethBalance)
+  console.log(`WETH Balance: ${wethBalanceInEther} WETH`)
+
+  // get axie id from  command line args
+  const args = process.argv.slice(2)
+  const axieId = parseInt(args[0])
+
+  const skymavisApyKey = process.env.SKIMAVIS_DAPP_KEY
+  const receipt = await buyMarketplaceOrder(axieId, wallet, provider, skymavisApyKey)
+  console.log(receipt.transactionHash)
+}
+
+buyAxie()
